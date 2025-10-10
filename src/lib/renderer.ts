da1847505cec0037e6708d34465e8d1acd57e691
jiyongkwack <rhkrwldyd@gmail.com>
2025-10-10 23:50:34 +0900

    feat: [renderer] 분홍색 다음 팔레트 연결되도록 수정

--- src/lib/renderer.ts
+++ src/lib/renderer.ts
@@ -32,7 +32,9 @@ export async function renderToPng(params: RenderParams): Promise<Buffer> {
 
   const palHead = PAL.head[rowHead._u2] ?? PAL.head[0];
   const palBody =
-    params.bodyc >= 255 ? PAL.body[12] : (PAL.body[rowBody._u2] ?? PAL.body[0]);
+    params.bodyc >= 255
+      ? PAL.body[11 + params.bodyc / 255]
+      : (PAL.body[rowBody._u2] ?? PAL.body[0]);
   const palShld = PAL.shield[rowShld._u2] ?? PAL.shield[0];
 
   const bitmaps = [] as ReturnType<typeof decodeEpfItem>[];
