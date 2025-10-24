b1e85c4e58b4fa4de7efc12429d96b291ddb1f81
jiyongkwack <rhkrwldyd@gmail.com>
2025-10-25 01:12:59 +0900

    hofix: [renderer] gifEncoder import 관련 이슈 수정

--- src/lib/gifEncoder.ts
+++ src/lib/gifEncoder.ts
@@ -1,5 +1,7 @@
 // gif.service.ts
-import GIFEncoder from 'gif-encoder-2';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import GIFEncoder = require('gif-encoder-2');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 import { PNG } from 'pngjs';
 // import { fillTransparentInPngBuffer } from './pngEncoder';
 
