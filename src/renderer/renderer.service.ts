8057487555e2d1b02fde6d309d43623fdeb9cf7e
jiyongkwack <rhkrwldyd@gmail.com>
2025-11-02 11:56:33 +0900

    improvement: [renderer]  의상실 업데이트 - 의상실 애니메이션 랜더링시 apng 로 저장되도록 수정 - 모션 동작 중에도 무기염색 애니메이션 동작하도록 수정

--- src/renderer/renderer.service.ts
+++ src/renderer/renderer.service.ts
@@ -3,57 +3,109 @@
 import { Injectable } from '@nestjs/common';
 import { renderToPng } from '../lib/renderer';
 import { RenderParams } from '../lib/types';
-import { GifService } from 'src/lib/gifEncoder';
 import { makeAPNGBase64 } from 'src/lib/apngEncoder';
 
 @Injectable()
 export class RendererService {
   async render(params: RenderParams) {
-    // return await renderToPng(params);
-    if (params.isAction) {
-      if (
-        [
-          0, 3, 6, 9, 12, 15, 18, 21, 32, 35, 38, 41, 52, 55, 58, 61, 80, 83,
-          86, 89,
-        ].includes(params.frame)
-      ) {
-        const pngBuffers = await Promise.all([
-          renderToPng(params),
-          renderToPng({ ...params, frame: params.frame + 1 }),
-          renderToPng(params),
-          renderToPng({ ...params, frame: params.frame + 2 }),
-        ]);
-        return GifService().makeGifFromPngBuffers(pngBuffers);
-      } else if ([25, 27, 29, 31, 45, 47, 49, 51].includes(params.frame)) {
-        const pngBuffers = await Promise.all([
-          renderToPng(params),
-          renderToPng({ ...params, frame: params.frame - 1 }),
-        ]);
-        return GifService().makeGifFromPngBuffers(pngBuffers, {
-          delayMs: 333,
-          optimized: false,
-        });
-      } else {
-        return await renderToPng(params);
+    // 애니메이션 계열인 경우
+    if ([255, 510, 765].includes(params.weaponc)) {
+      if (params.isAction) {
+        if (
+          [
+            0, 3, 6, 9, 12, 15, 18, 21, 32, 35, 38, 41, 52, 55, 58, 61, 80, 83,
+            86, 89,
+          ].includes(params.frame)
+        ) {
+          const pngBuffers = await Promise.all(
+            // 32 프레임
+            Array.from({ length: 32 }, (_, i) => i)
+              .reverse()
+              .map((x: number) => {
+                return x % 2 == 0
+                  ? renderToPng({ ...params, weaponAnic: x })
+                  : renderToPng({
+                      ...params,
+                      weaponAnic: x,
+                      frame: params.frame + (x % 4 === 1 ? 1 : 2),
+                    });
+              }),
+          );
+          return makeAPNGBase64(
+            pngBuffers.map((b) => {
+              return { png: b, delayNum: 3.3 };
+            }),
+            { loopCount: 0 },
+          );
+        } else if ([25, 27, 29, 31, 45, 47, 49, 51].includes(params.frame)) {
+          const pngBuffers = await Promise.all(
+            // 32 프레임
+            Array.from({ length: 32 }, (_, i) => i)
+              .reverse()
+              .map((x: number) => {
+                return x % 2 == 0
+                  ? renderToPng({ ...params, weaponAnic: x })
+                  : renderToPng({
+                      ...params,
+                      weaponAnic: x,
+                      frame: params.frame - 1,
+                    });
+              }),
+          );
+          return makeAPNGBase64(
+            pngBuffers.map((b) => {
+              return { png: b, delayNum: 3.3 };
+            }),
+            { loopCount: 0 },
+          );
+        }
       }
+      const pngBuffers = await Promise.all(
+        // 32 프레임
+        Array.from({ length: 32 }, (_, i) => i)
+          .reverse()
+          .map((x: number) => renderToPng({ ...params, weaponAnic: x })),
+      );
+      return makeAPNGBase64(
+        pngBuffers.map((b) => {
+          return { png: b, delayNum: 3.3 };
+        }),
+        { loopCount: 0 },
+      );
     } else {
-      if ([255, 510, 765].includes(params.weaponc)) {
-        // 애니메이션 계열인 경우
-        const pngBuffers = await Promise.all(
-          // 32 프레임
-          Array.from({ length: 32 }, (_, i) => i)
-            .reverse()
-            .map((x: number) => renderToPng({ ...params, weaponAnic: x })),
-        );
-        return makeAPNGBase64(
-          pngBuffers.map((b) => {
-            return { png: b, delayNum: 5 };
-          }),
-          { loopCount: 0 },
-        );
-      } else {
-        return await renderToPng(params);
+      if (params.isAction) {
+        if (
+          [
+            0, 3, 6, 9, 12, 15, 18, 21, 32, 35, 38, 41, 52, 55, 58, 61, 80, 83,
+            86, 89,
+          ].includes(params.frame)
+        ) {
+          const pngBuffers = await Promise.all([
+            renderToPng(params),
+            renderToPng({ ...params, frame: params.frame + 1 }),
+            renderToPng(params),
+            renderToPng({ ...params, frame: params.frame + 2 }),
+          ]);
+          return makeAPNGBase64(
+            pngBuffers.map((b) => {
+              return { png: b, delayNum: 3.3 };
+            }),
+            { loopCount: 0 },
+          );
+        } else if ([25, 27, 29, 31, 45, 47, 49, 51].includes(params.frame)) {
+          const pngBuffers = await Promise.all([
+            renderToPng(params),
+            renderToPng({ ...params, frame: params.frame - 1 }),
+          ]);
+          return makeAPNGBase64(
+            pngBuffers.map((b) => {
+              return { png: b, delayNum: 3.3 };
+            }),
+            { loopCount: 0 },
+          );
+        }
       }
+      return await renderToPng(params);
     }
   }
 }
