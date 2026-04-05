539c8efbf9abd03a373143b3d717d0958b5b35f9
jiyongkwack <rhkrwldyd@gmail.com>
2026-04-05 22:16:34 +0900

    feat: [user] 필요없어진 api 와 스키마 삭제

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,122 +1,55 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import {
-  Body,
   Controller,
   Get,
   HttpStatus,
-  Param,
   Post,
-  Query,
-  Req,
   Res,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { UserService } from './user.service';
-import { V2User } from './v2-user.schema';
+import { Response } from 'express';
 
-const UPSERT_SECRET =
-  '5fa092bc12fb7c75200b7dd18526c7af9f664e49accdb8a51a377695f165f6736fc62402ff89a227fdff9fd4b0c07fb78a8a559356afda9f06b1c89005a2d4717e114d9bdb7daa1a837e9e29dbb6fb342819694bc90775512fb357471c59c388f87acde1d6823862ee678822bf89ed5619a11a7b421f4d0adf8b6b20f13f7534';
+const SEARCH_DISABLED_MESSAGE = 'user search features are disabled';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly svc: UserService) {}
-
   @Get('/')
-  async getUserData(@Query('name') name: string, @Req() req: Request) {
-    const users = await this.svc.findUserByName(name, this.extractRequestIp(req));
-
-    return users.map((user) => ({
-      name: user.Name,
-      clan: user.ClanName,
-      class: user.Class,
-      nation: user.Nation,
-      level: user.Level,
-      grade: user.Grade,
-      hp: user.MaxHP,
-      mp: user.MaxMP,
-      likeCount: user.likeCount,
-      isHidden: user.isHidden,
-    }));
+  legacyUserSearch(@Res() res: Response) {
+    this.sendGone(res);
   }
 
   @Get('/single')
-  async getSingleUserData(@Query('name') name: string, @Req() req: Request) {
-    const user = await this.svc.findSingleUserByName(name, this.extractRequestIp(req));
-
-    return {
-      name: user.Name,
-      clan: user.ClanName,
-      class: user.Class,
-      nation: user.Nation,
-      level: user.Level,
-      grade: user.Grade,
-      hp: user.MaxHP,
-      mp: user.MaxMP,
-      likeCount: user.likeCount,
-      isHidden: user.isHidden,
-    };
+  legacySingleUserSearch(@Res() res: Response) {
+    this.sendGone(res);
   }
 
   @Get('/clan')
-  async getUsersByClan(@Query('name') name: string) {
-    const users = await this.svc.findUsersByClanName(name);
-
-    return users.map((user) => ({
-      name: user.Name,
-      clan: user.ClanName,
-      class: user.Class,
-      nation: user.Nation,
-      level: user.Level,
-      grade: user.Grade,
-      hp: user.MaxHP,
-      mp: user.MaxMP,
-      likeCount: user.likeCount,
-      isHidden: user.isHidden,
-    }));
+  legacyClanSearch(@Res() res: Response) {
+    this.sendGone(res);
   }
 
   @Get('/search-ranking')
-  async getSearchRanking() {
-    return this.svc.getSearchRanking(5);
+  legacySearchRanking(@Res() res: Response) {
+    this.sendGone(res);
   }
 
   @Post('/:name/like')
-  async likeCharacter(@Param('name') name: string, @Req() req: Request) {
-    return this.svc.addCharacterLike(name, this.extractRequestIp(req));
+  legacyCharacterLike(@Res() res: Response) {
+    this.sendGone(res);
   }
 
   @Post('/userDatas')
   legacyUserDatas(@Res() res: Response) {
-    res.status(HttpStatus.GONE).send({
-      result: 'fail',
-      message: 'legacy users updates are disabled',
-    });
+    this.sendGone(res);
   }
 
   @Post('/v2-userDatas')
-  async v2UserDatas(
-    @Body('userDatas') userDatas: Array<V2User>,
-    @Body('secret') secret: string,
-    @Res() res: Response,
-  ) {
-    if (secret === UPSERT_SECRET) {
-      await this.svc.upsertV2Users(userDatas);
-      res.send({ result: 'ok' });
-      return;
-    }
-
-    res.send({ result: 'fail' });
+  legacyV2UserDatas(@Res() res: Response) {
+    this.sendGone(res);
   }
 
-  private extractRequestIp(req: Request) {
-    const forwardedFor = req.headers['x-forwarded-for'];
-    const firstForwardedIp =
-      typeof forwardedFor === 'string'
-        ? forwardedFor.split(',')[0]
-        : Array.isArray(forwardedFor)
-          ? forwardedFor[0]
-          : null;
-    const rawIp = (firstForwardedIp ?? req.ip ?? req.socket.remoteAddress ?? '').trim();
-    return rawIp.replace(/^::ffff:/, '') || 'unknown';
+  private sendGone(res: Response) {
+    res.status(HttpStatus.GONE).send({
+      result: 'fail',
+      message: SEARCH_DISABLED_MESSAGE,
+    });
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,25 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CharacterLikeSchema } from './character-like.schema';
-import { CharacterSearchSchema } from './character-search.schema';
 import { UserController } from './user.controller';
-import { UserSchema } from './user.schema';
 import { UserService } from './user.service';
-import { V2UserSchema } from './v2-user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature(
       [
-        { name: 'users', schema: UserSchema },
-        { name: 'v2_users', schema: V2UserSchema },
         {
           name: 'character_likes',
           schema: CharacterLikeSchema,
-        },
-        {
-          name: 'character_searches',
-          schema: CharacterSearchSchema,
         },
       ],
       'barambook',
